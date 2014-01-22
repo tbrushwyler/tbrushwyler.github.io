18626bc19d4a09e2a929148d5da027b7cba5a39a
Taylor Brushwyler <trb1992@gmail.com>
2014-01-22 01:30:27 -0500

    Remove reliance on crawlers. Instead, use the json from the sites.

--- news/DNCtrl.js
+++ news/DNCtrl.js
@@ -2,22 +2,11 @@
 
 app.controller('DNCtrl', ['$scope', '$http', function($scope, $http) {
 	$scope.retrieve = function() {
-		importio.query({"connectorGuids": [
-          "3e56a363-947e-4f9e-8eb1-198c99a18704"
-        ],
-        "input": {
-          "webpage/url": "https://news.layervault.com/stories"
-        },
-    	"asObjects": true}, {
-		  "data": function(data) {
-		    $scope.items = data;
-		  }
-		});
-		// $http.jsonp("http://www.kimonolabs.com/api/7vtk89ba?apikey=823d413e416e3b686a51c2262afc4c8a&callback=dn_callback");
+		$http.jsonp("http://json2jsonp.com/?url=https://news.layervault.com/stories.json&callback=dn_callback");
 	}
 
 	window.dn_callback = function(data) {
-		$scope.items = data.results.collection1;
+		$scope.items = data.stories;
 	}
 
 	if (!$scope.items) {
--- news/HNCtrl.js
+++ news/HNCtrl.js
@@ -2,11 +2,11 @@
 
 app.controller('HNCtrl', ['$scope', '$http', function($scope, $http) {
 	$scope.retrieve = function() {
-		$http.jsonp("http://www.kimonolabs.com/api/3qleob74?apikey=823d413e416e3b686a51c2262afc4c8a&callback=hn_callback");
+		$http.jsonp("http://api.ihackernews.com/page?format=jsonp&callback=hn_callback");
 	}
 
 	window.hn_callback = function(data) {
-		$scope.items = data.results.collection2;
+		$scope.items = data.items;
 	}
 
 	if (!$scope.items) {
